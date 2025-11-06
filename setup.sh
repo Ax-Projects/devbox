9be022a8313bc6bc7a9a863a8081a2335323c2a4
Ax-Projects <101007656+Ax-Projects@users.noreply.github.com>
2025-11-06 19:36:54 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# My own portable dev environment - with Portable Nix
+
+# Prepare Neovim and BashRC
+
+curl -fsSL https://get.jetify.com/devbox | bash && \
+devbox init -q && \
+cat > devbox.json <<EOF
+{
+  "$schema": "https://raw.githubusercontent.com/jetify-com/devbox/0.16.0/.schema/devbox.schema.json",
+  "env": { "PROJECT_DIR": "$PWD" },
+  "packages": [
+	"git@latest"
+    "fd@latest",
+    "fish@latest",
+    "fzf@latest",
+    "ripgrep@latest",
+    "bat@latest",
+    "btop@latest",
+    "yazi@latest",
+    "neovim@latest",
+    "lazydocker@latest",
+    "lazygit@latest"
+  ],
+  "shell": {
+    "init_hook": [
+      "echo 'Amsalem Devbox!'",
+      "source /tmp/amsrc"
+    ],
+    "scripts": {
+      "set-nvim": [
+        "bash ams-set-nvim.sh"
+      ]
+    }
+  }
+}
+EOF
+
+cat <<EOF > /tmp/amsrc
+function y() {
+	local tmp='$(mktemp -t "yazi-cwd.XXXXXX")' cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+EOF
+
+cat > ams-set-nvim.sh << EOF
+mkdir -p /tmp/ams-nvim && \
+if [ ! -d ~/.config/nvim ]; then
+ln -sf /tmp/ams-nvim ~/.config/nvim && \
+git clone https://github.com/LazyVim/starter ~/.config/nvim && \
+rm -rf ~/.config/nvim/.git && \
+else
+echo 'NeoVim config folder exists on this host. Skipping nvim setup'
+fi
+EOF
+
+cat > ams-cleanup.sh << EOF
+sudo rm -rf /nix
+sudo rm -rf ~/.nix*
+sudo rm /usr/local/bin/devbox
+cd ~/.config && sudo rm -rf devbox fish yazi nvim /tmp/ams-nvim
+EOF
+
+chmod u+x ams-set-nvim.sh ams-cleanup.sh
+devbox install -q && \
+devbox run set-nvim && \
+devbox shell
